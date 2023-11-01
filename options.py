51203ecf247bb75354903da84ef9ba5b1a39c17e
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-11-01 20:39:16 +0100

    add title and icon option for window

--- options.py
+++ options.py
@@ -1,5 +1,7 @@
 import pygame
 
+title = "Game Title"
+icon = ""
 # width height of window
 pygame.display.set_mode((400, 500))
 
@@ -9,3 +11,9 @@ surface = pygame.display.get_surface()
 color = (20, 20, 20)
 surface.fill(color)
 pygame.display.flip()
+
+# setting window name
+pygame.display.set_caption(title)
+if icon != "":
+    IconImage = pygame.image.load(icon)
+    pygame.display.set_icon(IconImage)
