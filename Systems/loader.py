42e186a415e76a48bcab73718579d0d0793d3566
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-11-20 19:12:07 +0100

    change loader class and change path variable
    path variable was changed to user specyfic
    loader class was changed to work more like static class and not as an object

--- Systems/loader.py
+++ Systems/loader.py
@@ -1,32 +1,22 @@
-from pygame import *
-
+from pygame import image
+import os
 
 class Loader:
-    def __init__(self, path_to_folder="C:/Users/popla/PycharmProjects/pygame/Pygame_project/Animations"):
-        self.path_to_folder = path_to_folder
-        self._image = set()
+    ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    @classmethod
+    def load_image(cls, name, type):
+        print(cls.ROOT_FOLDER)
+        img = image.load(cls.path_to_folder+name+type)
+        return img
 
-    def load_image(self, name, type):
-        if img := self.search_image(name) is not None:
-            img = image.load(self.path_to_folder+name+type)
-            self._image.add({name: name, sprite: img})
-            return img
-        else:
-            return img
-
-    def load_image_array(self, path_to_animation, type):
+    @classmethod
+    def load_image_array(cls, path_to_animation, type):
         images = []
         counter = 0
         while True:
             try:
-                images.append(image.load(f"{self.path_to_folder}{path_to_animation}/{counter}.{type}"))
+                images.append(image.load(f"{cls.path_to_folder}{path_to_animation}/{counter}.{type}"))
                 counter += 1
             except FileNotFoundError:
                 break
         return images
-
-    def search_image(self, name):
-        for img in self._image:
-            if img.name == name:
-                return img.sprite
-        return None
