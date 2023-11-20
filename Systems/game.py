6b3df7e8173f9531caf55fac3e27bdd753297315
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-11-20 21:59:35 +0100

    change game to add pre code for running

--- Systems/game.py
+++ Systems/game.py
@@ -1,6 +1,18 @@
 from Systems.map import Map
 from Systems.loader import Loader
+
+
 class Game:
-    def __init__(self):
+    def __init__(self, root):
+        self.root_dir = root
         self.map_manager = Map()
-        img = Loader.load_image("/div", "png")
+        self.loader = Loader(root)
+        self.running = False
+
+    def start_game(self):
+        self.running = True
+        while self.running:
+            ...
+
+    def update(self):
+        ...
