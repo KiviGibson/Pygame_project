b0115709de2e6932c87a9e7d634060f737098223
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-01-03 10:03:48 +0100

    add sounds to players

--- Game/Objects/Player/player.py
+++ Game/Objects/Player/player.py
@@ -15,6 +15,14 @@ class Player(gameobject.GameObject):
             "idle": loader.Loader(df.ROOT_PATH).load_image_array("/Images/Animations/green/idle", "png"),
             "walk": loader.Loader(df.ROOT_PATH).load_image_array("/Images/Animations/green/walk", "png")
         }
+        self.sounds = {
+            "jump": loader.Loader(df.ROOT_PATH).load_sound("/Sounds/jump.wav"),
+            "walk": loader.Loader(df.ROOT_PATH).load_sound("/Sounds/walk.wav"),
+            "hit_ground": loader.Loader(df.ROOT_PATH).load_sound("/Sounds/hit_ground.wav")
+        }
+        self.sounds["walk"].set_volume(0.05)
+        self.sounds["hit_ground"].set_volume(0.3)
+        self.sounds["jump"].set_volume(0.3)
         super().__init__(size, position, img=self.images["idle"][0])
         self.mov_x: float = 0
         self.mov_y: float = 0
@@ -41,6 +49,7 @@ class Player(gameobject.GameObject):
         else:
             self.can_jump -= 1
         self.collider.update(self.x, self.y)
+        self.stomp()
         self.animate(x)
 
     def press_button(self, button: int) -> None:
@@ -80,6 +89,8 @@ class Player(gameobject.GameObject):
                     on_g = True
                 if second_side:
                     y_dir = max(y_dir, 0)
+        if self.on_ground != on_g and on_g is True:
+            self.play("hit_ground")
         self.on_ground = on_g
         self.mov_y = y_dir
         return x_dir, y_dir
@@ -96,6 +107,7 @@ class Player(gameobject.GameObject):
 
     def jump(self) -> None:
         if self.can_jump > 0:
+            self.play("jump")
             self.can_jump = 0
             self.mov_y = -12.8
 
@@ -103,7 +115,7 @@ class Player(gameobject.GameObject):
         self.rect.x = self.x
         self.rect.y = self.y
 
-    def animate(self, x):
+    def change_animations(self,x):
         if x != 0:
             self.state = "walk"
             if x < 0:
@@ -111,9 +123,18 @@ class Player(gameobject.GameObject):
             else:
                 self.side = False
         else:
-            self.state="idle"
+            self.state = "idle"
+            
+    def animate(self, x):
+        self.change_animations(x)
         max_img = len(self.images[self.state])
         self.image = self.images[self.state][int(self.image_index) % max_img]
         self.image = pygame.transform.flip(self.image, self.side, False)
         self.image_index += 0.2
         self.image_index %= max_img
+    def stomp(self):
+        if self.state == "walk" and round(self.image_index, 2) % 3.0 == 0 and self.on_ground is True:
+            self.play("walk")
+
+    def play(self, name: str) -> None:
+        self.sounds[name].play()
