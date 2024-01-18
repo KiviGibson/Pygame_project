d988155d72d0fae8a54804c5a66aa432818b3645
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-01-18 09:23:56 +0100

    exchange code with gameobject and simobject

--- Game/Objects/Player/player.py
+++ Game/Objects/Player/player.py
@@ -1,38 +1,32 @@
 import pygame
 
-import Game.game as game
-import definition as df
 import Game.loader as loader
-from Game.Objects import gameobject
-from Game.Objects.Player.squere_collider import SquereCollider
+import Game.Objects.collisionobject as collisionobject
 
 
-class Player(gameobject.GameObject): 
-    SPEED = 4
-
+class Player(collisionobject.CollisionObject):
     def __init__(self, size=(30, 36), position=(0, 0)) -> None:
         self.images = {
-            "idle": loader.Loader(df.ROOT_PATH).load_image_array("/Images/Animations/green/idle", "png"),
-            "walk": loader.Loader(df.ROOT_PATH).load_image_array("/Images/Animations/green/walk", "png")
+            "idle": loader.Loader().load_image_array("/Images/Animations/green/idle", "png"),
+            "walk": loader.Loader().load_image_array("/Images/Animations/green/walk", "png"),
+            "damage": loader.Loader().load_image_array("/Images/Animations/green/damage", "png")
         }
         self.sounds = {
-            "jump": loader.Loader(df.ROOT_PATH).load_sound("/Sounds/jump.wav"),
-            "walk": loader.Loader(df.ROOT_PATH).load_sound("/Sounds/walk.wav"),
-            "hit_ground": loader.Loader(df.ROOT_PATH).load_sound("/Sounds/hit_ground.wav")
+            "jump": loader.Loader().load_sound("/Sounds/jump.wav"),
+            "walk": loader.Loader().load_sound("/Sounds/walk.wav"),
+            "hit_ground": loader.Loader().load_sound("/Sounds/hit_ground.wav"),
+            "hurt": loader.Loader().load_sound("/Sounds/damege.wav")
         }
-        self.sounds["walk"].set_volume(0.05)
+        self.sounds["walk"].set_volume(0.3)
         self.sounds["hit_ground"].set_volume(0.3)
         self.sounds["jump"].set_volume(0.3)
-        super().__init__(size, position, img=self.images["idle"][0])
-        self.mov_x: float = 0
-        self.mov_y: float = 0
-        self.collider: SquereCollider = SquereCollider(size, (self.x, self.y), self)
+        super().__init__(size, position, img=self.images["idle"][0], simulate=True)
         self.can_jump: int = 0
         self.state = "idle"
         self.side: bool = False
         self.image_index: float = 0
         self.coyote_time_duration: int = 5
-        self.on_ground: bool = True
+        self.on_ground: bool = False
         self.interact: bool = False
 
     def update(self, g) -> None:
@@ -41,9 +35,9 @@ class Player(gameobject.GameObject):
                 self.press_button(event.key)
             elif event.type == pygame.KEYUP:
                 self.release_button(event.key)
-        x, y = self.calculate_movement()
-        x, y = self.collide(g, x, y)
-        self.move(x, y)
+        x = self.calculate_movement()
+        self.mov_y = self.gravity(self.mov_y, self.on_ground)
+        self.move(x, self.mov_y)
         if self.on_ground:
             self.can_jump = self.coyote_time_duration
         else:
@@ -72,50 +66,13 @@ class Player(gameobject.GameObject):
             case pygame.K_e:
                 self.interact = False
 
-    def collide(self, g, x_dir, y_dir) -> tuple[float, float]:
-        on_g = False
-        for obj in self.collider.collide_with(g):
-            x, y, first_side, second_side = self.collider.check_direction(obj.collider)
-            try:
-                self.x = float(x)
-                if first_side:
-                    x_dir = min(x_dir, 0)
-                if second_side:
-                    x_dir = max(x_dir, 0)
-            except TypeError:
-                self.y = float(y)
-                if first_side:
-                    y_dir = min(y_dir, 0)
-                    on_g = True
-                if second_side:
-                    y_dir = max(y_dir, 0)
-        if self.on_ground != on_g and on_g is True:
-            self.play("hit_ground")
-        self.on_ground = on_g
-        self.mov_y = y_dir
-        return x_dir, y_dir
-
-    def calculate_movement(self) -> tuple[float, float]:
-        y = self.mov_y + game.Game.GRAVITY
-        x = self.mov_x * Player.SPEED
-        return x, y
-
-    def move(self, x, y) -> None:
-        self.x += x
-        self.y += y
-        self.change_sprite_pos()
-
-    def jump(self) -> None:
-        if self.can_jump > 0:
+    def jump(self, force=False) -> None:
+        if self.can_jump > 0 or force:
             self.play("jump")
             self.can_jump = 0
             self.mov_y = -12.8
 
-    def change_sprite_pos(self) -> None:
-        self.rect.x = self.x
-        self.rect.y = self.y
-
-    def change_animations(self,x):
+    def change_animations(self, x):
         if x != 0:
             self.state = "walk"
             if x < 0:
@@ -132,9 +89,14 @@ class Player(gameobject.GameObject):
         self.image = pygame.transform.flip(self.image, self.side, False)
         self.image_index += 0.2
         self.image_index %= max_img
+
     def stomp(self):
         if self.state == "walk" and round(self.image_index, 2) % 3.0 == 0 and self.on_ground is True:
             self.play("walk")
 
     def play(self, name: str) -> None:
         self.sounds[name].play()
+
+    def damage(self, g):
+        self.sounds["hurt"].play()
+        g.reload_map()
