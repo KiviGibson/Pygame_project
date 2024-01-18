9fba6ec46af21633759091c9589434e8b5d38cf9
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-01-18 09:22:52 +0100

    refactor code
    cleanup errors that was made when collision was refactored

--- Game/Objects/Gate/gate.py
+++ Game/Objects/Gate/gate.py
@@ -12,14 +12,15 @@ class Gate(gameobject.GameObject):
         self.map = map
         self.spawn = spawn
         self.collider = squere_collider.SquereCollider(size, position, self, trigger=True)
-        img = loader.Loader(df.ROOT_PATH).load_image("/Images/arrow_right_curve", "png")
+        img = loader.Loader().load_image("/Images/arrow_right_curve", "png")
         ui = pygame.surface.Surface((32, 32), pygame.SRCALPHA)
         pygame.transform.scale(img, (32, 32), ui)
         self.ui_icon = gameobject.GameObject((32, 32), (position[0]-7, position[1]-32), img=ui)
 
-    def on_collision(self, other) -> None:
+    def on_trigger(self, other) -> None:
+        print("trigger!")
         try:
-            if other.parent.interact:
+            if other.interact:
                 self.game.change_map(self.map, self.spawn)
         except AttributeError:
             pass
