f7bf44cb0eb7f70b206b20e5bd2281e21a826219
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-02-17 21:34:08 +0100

    add first function to use as interactable

--- Game/Objects/MovingObject/platform.py
+++ Game/Objects/MovingObject/platform.py
@@ -1,20 +1,26 @@
+import definition
 import Game.Objects.collisionobject as gm
 import Game.loader as loader
 import pygame
 
 
 class Platform(gm.CollisionObject):
-    def __init__(self, pos, segments, side):
-        super().__init__(pos)
+    def __init__(self, pos, segments, index):
         self.images = {
             "start": loader.Loader().load_image("/Images/platform/start", "png"),
             "mid": loader.Loader().load_image("/Images/platform/mid", "png"),
             "end": loader.Loader().load_image("/Images/platform/end", "png"),
             "single": loader.Loader().load_image("/Images/platform/single", "png")
         }
-        self.create_platform(segments, side)
+        self.funcs = {
+            "move": lambda p: self.change_pos(p)
+        }
+        super().__init__((18 * segments, 18), pos)
+        self.create_platform(segments)
+        self.postarget = [self.x, self.y]
+        self.index = index
 
-    def create_platform(self, segments, side):
+    def create_platform(self, segments):
         self.image = pygame.surface.Surface((18 * segments, 18))
         if segments == 1:
             self.image.blit(self.images["single"], (0, 0))
@@ -23,3 +29,20 @@ class Platform(gm.CollisionObject):
             for i in range(segments-1):
                 self.image.blit(self.images["mid"], ((i+1)*18, 0))
             self.image.blit(self.images["end"], ((segments-1)*18, 0))
+
+    def change_pos(self, pos: str):
+        self.postarget = list(pos.split(" "))
+        self.postarget[0] = int(self.postarget[0]) * 18 + self.x
+        self.postarget[1] = int(self.postarget[1]) * 18 + self.y
+
+    def update(self, game):
+        self.move()
+        self.collider.update(self.x, self.y)
+
+    def on_collision(self, other):
+        ...
+
+    def move(self) -> None:
+        self.y = definition.lerp(self.y, self.postarget[1], 0.5)
+        self.y = round(self.y, 2)
+        super().move(0,0)
